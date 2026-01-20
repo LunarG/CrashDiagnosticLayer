faaea20cfe19407cd12279c4bbaa75314e91a3c6
Christophe <christophe@lunarg.com>
2026-01-20 17:37:13 +0100

    cdl: Control timeline semaphore crash detection

--- src/cdl.h
+++ src/cdl.h
@@ -108,6 +108,7 @@ struct Settings {
     bool trace_all_semaphores{false};
     bool trace_all{false};
     bool sync_after_commands{false};
+    bool trigger_timeline_semaphore{true};
     bool trigger_watchdog_timer{true};
     uint64_t watchdog_timer_ms{30000};
 };
